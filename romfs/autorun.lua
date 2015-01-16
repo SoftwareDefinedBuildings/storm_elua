275b07c52e529a9005000a1959073011a812f3b2
Gabe Fierro <gtfierro225@gmail.com>
2015-01-15 20:12:18 -0800

    use new nodeid function, comment out mpdb stuff until we want it

--- romfs/autorun.lua
+++ romfs/autorun.lua
@@ -1,35 +1,36 @@
 
---require("storm") -- libraries for interfacing with the board and kernel
+require("storm") -- libraries for interfacing with the board and kernel
 require("cord") -- scheduler / fiber library
 require("mpdb") -- msgpack database
-
 
 sock = storm.net.bind(101, function(payload, from, port)
     print ("Got a message from ",from, " port ",port)
     print ("Payload: ", payload)
 end)
 
+print("Node ID", storm.os.nodeid())
 
+--[[
 mpdb.connect("2001:470:4885:1:9::", 7000)
 
 count = 0
 storm.os.invokePeriodically(5*storm.os.SECOND, function()
     --storm.net.sendto(socket_handle, buffer, addrstr, port)
-    --print ("sending")
     packet = {
         key = "mykey",
         value = "here's a value",
         count = count
     }
-    mpdb.insert(packet)
     count = count + 1
     if count % 4 == 0 then
-        print (" oh")
         query = {
-           count = 40
+           count = 10
         }
         mpdb.query(query)
+    else
+        mpdb.insert(packet)
     end
 end)
+--]]
 
 cord.enter_loop() -- start event/sleep loop
--- romfs/mpdb.lua
+++ romfs/mpdb.lua
@@ -10,12 +10,17 @@ mpdb.connect = function (addr, port)
     mpdb._server_port = port
     mpdb._sock = storm.net.bind(200, function(payload, from, port)
         print ("Got a message from ",from, " port ",port)
-        print ("Payload: ", payload)
+        done = storm.mp.unpack(payload)
+        for i, tab in ipairs(done["results"]) do 
+            print("result", i)
+            for k,v in pairs(tab) do print ("k/v",k,v) end
+        end
+        for key,value in pairs(done) do print ("k/v",key,value) end
     end)
 end
 
 mpdb.insert = function (table)
-    msg = {
+    local msg = {
         nodeid = 0xfeeb,
         oper = "INSERT",
         echo = 1,
@@ -24,8 +29,18 @@ mpdb.insert = function (table)
     storm.net.sendto(mpdb._sock, storm.mp.pack(msg), mpdb._server_addr, mpdb._server_port)
 end
 
+mpdb.persist = function (table)
+    local msg = {
+        nodeid = 0xfeeb,
+        oper = "PERSIST",
+        echo = 1,
+        data = table
+    }
+    storm.net.sendto(mpdb._sock, storm.mp.pack(msg), mpdb._server_addr, mpdb._server_port)
+end
+
 mpdb.query = function (where)
-    msg = {
+    local msg = {
         nodeid = 0xfeeb,
         oper = "QUERY",
         echo = 1,
