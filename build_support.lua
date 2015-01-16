ae397b123d81f900eaec18f4768dac3a69136a59
Michael Andersen <m.andersen@cs.berkeley.edu>
2015-01-16 14:56:40 -0800

    Cleanup for class build system

---
+++ build_support.lua
@@ -0,0 +1,27 @@
+local lfs = require "lfs"
+function go_build()
+    if autoupdate == true then
+        cwd = lfs.currentdir()
+        lfs.chdir("toolchains/storm_elua")
+        os.execute("rm -f romfs/*")
+        os.execute("git pull")
+        lfs.chdir(cwd)
+    end
+    os.execute("rm -f toolchains/storm_elua/romfs/*")
+    if autorun ~= nil then
+        if lfs.attributes(autorun) == nil then
+            print ("could not load autorun file")
+            return
+        end
+        lfs.link("toolchains/storm_elua/romfs/autorun.lua", autorun)
+    end
+    for k,v in pairs(libs) do
+        if lfs.attributes(v) == nil then
+            print (string.format("could not load library '%s'",k))
+            return
+        end
+        lfs.link(string.format("toolchains/storm_elua/romfs/%s.lua",k),v)
+    end
+    lfs.chdir("toolchains/storm_elua")
+    os.execute("./gobuild")
+end
